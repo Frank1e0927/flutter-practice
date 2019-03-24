8149544fc181355808e374df11c437e6d49db68d
frankie-mbp <877672473@qq.com>
2019-03-24 21:30:42 +0800

    tabs learning

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_practice/routes/FirstScreen.dart';
+import 'package:flutter_practice/routes/SecondScreen.dart';
+import 'package:flutter_practice/routes/ThirdScreen.dart';
 
 void main() => runApp(MyApp());
 
@@ -20,7 +23,54 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: new MyHome(),
+    );
+  }
+}
+
+class MyHome extends StatefulWidget {
+  @override
+  MyHomeState createState() => new MyHomeState();
+}
+
+class MyHomeState extends State<MyHome> with SingleTickerProviderStateMixin {
+  TabController controller;
+
+  @override
+  void initState() {
+    super.initState();
+    controller = new TabController(length: 3, vsync: this);
+  }
+
+  @override
+  void dispose() {
+    controller.dispose();
+    super.dispose();
+  }
+  
+  TabBar getTabBar() {
+    return new TabBar(tabs: <Tab>[
+      new Tab(icon: new Icon(Icons.favorite)),
+      new Tab(icon: new Icon(Icons.adb)),
+      new Tab(icon: new Icon(Icons.airport_shuttle),)
+    ],
+    controller: controller,
+    );
+  }
+  
+  TabBarView getTabBarView(var tabs) {
+    return new TabBarView(children: tabs, controller: controller);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return new Scaffold(
+      appBar: new AppBar(
+        title: new Text("using tabs"),
+        backgroundColor: Colors.blue,
+        bottom: getTabBar(),
+      ),
+      body: getTabBarView(<Widget>[new First(), new Second(), new Third()]),
     );
   }
 }
